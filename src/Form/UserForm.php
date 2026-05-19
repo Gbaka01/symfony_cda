5bb46973b38a579768ca6839ffd9286c935102ed
Gbaka01 <gore.goli@gmail.com>
2026-05-19 15:20:03 +0200

    commitgg24

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserForm extends AbstractType
 {
