db2a163717ff6d9b3490bc2fcde889a7d8cd5ee1
Gbaka01 <gore.goli@gmail.com>
2026-05-19 10:54:56 +0200

    commitgg21

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -42,6 +43,26 @@ class UserForm extends AbstractType
                     'class' => 'form-control'
                 ]
                 ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                ],
+                'constraints' => [
+                    new NotBlank(message: 'Veuillez entrer un mot de passe'),
+                    new Length(
+                        min: 8,
+                        minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                        max: 4096
+                    ),
+                    new Regex(
+                        pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/',
+                        message: 'Mot de passe trop faible : 1 minuscule, 1 majuscule, 1 chiffre, 1 caractère spécial, 8 caractères minimum.'
+                    ),
+                ],
+            ])    
             ->add('roles', ChoiceType::class, [
             'label' => 'roles',
             'choices' => [
